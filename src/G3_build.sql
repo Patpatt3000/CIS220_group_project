ac4dd3a9dd38d7823652e8cc07a6996a048b2d38
jordan aceto <jordanaceto@gmail.com>
2020-10-31 13:32:55 -0400

    remove circular dependency with an interesection

--- src/G3_build.sql
+++ src/G3_build.sql
@@ -3,24 +3,14 @@ DROP DATABASE IF EXISTS G3_Build;
 CREATE DATABASE         G3_Build;
 USE                     G3_Build;
 
-CREATE TABLE request_type (
-    id              INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
-    topic_label     VARCHAR(45)     NOT NULL,
-    application     VARCHAR(45)     NOT NULL,
-
-    PRIMARY KEY     (id)    
-);
-
 CREATE TABLE user (
     id              INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
-    department_id   INT UNSIGNED    NOT NULL,
     first_name      VARCHAR(45)     NOT NULL,
     last_name       VARCHAR(45)     NOT NULL,
     email           VARCHAR(45)     NOT NULL,
     phone           CHAR(12),
 
     PRIMARY KEY     (id)
-    /* department_id is a foreign key, but department doesn't exist yet */
 );
 
 CREATE TABLE department (
@@ -32,9 +22,22 @@ CREATE TABLE department (
     FOREIGN KEY     (manager_id) REFERENCES user(id)
 );
 
-/* add the FK to the user table only after the department table is created */
-ALTER TABLE user
-ADD FOREIGN KEY (department_id) REFERENCES department(id);
+CREATE TABLE department_users (
+    department_id   INT UNSIGNED    NOT NULL,
+    user_id         INT UNSIGNED    NOT NULL,
+
+    PRIMARY KEY     (department_id, user_id),
+    FOREIGN KEY     (department_id) REFERENCES department(id),
+    FOREIGN KEY     (user_id)       REFERENCES user(id)
+);
+
+CREATE TABLE request_type (
+    id              INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
+    topic_label     VARCHAR(45)     NOT NULL,
+    application     VARCHAR(45)     NOT NULL,
+
+    PRIMARY KEY     (id)    
+);
 
 CREATE TABLE department_request_type (
     department_id   INT UNSIGNED    NOT NULL,
@@ -98,16 +101,22 @@ VALUES ('Human Resources'),
        ('Control'),
        ('Blacksmithing');
 
-INSERT INTO user(department_id, first_name, last_name, email, phone)
-VALUES (6,' Anne-corinne','Bilton',   'abilton0@squidoo.com',     '850-947-3461'),
-       (1,' Casandra',    'Plowman',  'cplowman1@privacy.gov.au', '840-527-9292'),
-       (7,' Orsa',        'Dinnis',   'odinnis2@biglobe.ne.jp',   '516-185-6879'),
-       (7,' Jodi',        'Raoux',    'jraoux3@gov.uk',           '341-342-1095'),
-       (4,' Ken',         'Claydon',  'kclaydon4@nba.com',        '354-336-8754'),
-       (5,' Frederica',   'Coonan',   'fcoonan5@1688.com',        '725-863-6496'),
-       (1,' Orelle',      'Heare',    'oheare6@jiathis.com',      '952-241-4487'),
-       (2,' Henka',       'Brundill', 'hbrundill7@army.mil',      '475-792-9175'),
-       (5,' Baird',       'Belitz',   'bbelitz8@opensource.org',  '700-849-0437');
+INSERT INTO user(first_name, last_name, email, phone)
+VALUES ('Anne-corinne','Bilton',   'abilton0@squidoo.com',     '850-947-3461'),
+       ('Casandra',    'Plowman',  'cplowman1@privacy.gov.au', '840-527-9292'),
+       ('Orsa',        'Dinnis',   'odinnis2@biglobe.ne.jp',   '516-185-6879'),
+       ('Jodi',        'Raoux',    'jraoux3@gov.uk',           '341-342-1095'),
+       ('Ken',         'Claydon',  'kclaydon4@nba.com',        '354-336-8754'),
+       ('Frederica',   'Coonan',   'fcoonan5@1688.com',        '725-863-6496'),
+       ('Orelle',      'Heare',    'oheare6@jiathis.com',      '952-241-4487'),
+       ('Henka',       'Brundill', 'hbrundill7@army.mil',      '475-792-9175'),
+       ('Baird',       'Belitz',   'bbelitz8@opensource.org',  '700-849-0437');
+
+INSERT INTO department_users(department_id, user_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 5),
+       (7, 3);
 
 UPDATE department
 SET manager_id = 1
@@ -121,9 +130,11 @@ UPDATE department
 SET manager_id = 3
 WHERE name = 'Accounting';
 
+SELECT * FROM user;
+
 SELECT * FROM department;
 
-SELECT * FROM user;
+SELECT * FROM department_users;
 
 SELECT * FROM request_type;
 
